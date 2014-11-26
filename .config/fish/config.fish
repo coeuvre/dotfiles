6145edb7b1c2d6ca664e1f4f455722e8b681fbcc
Coeuvre <coeuvre@gmail.com>
2014-11-26 20:37:21 +0800

    Updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,48 @@
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme robbyrussell
+function _remote_hostname
+  if test -n "$SSH_CONNECTION"
+    echo (whoami)@(hostname)
+  end
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function fish_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
+
+  if test $last_status = 0
+      set arrow "$green➜ "
+  else
+      set arrow "$red➜ "
+  end
+  set -l cwd $cyan(_remote_hostname)' '(basename (prompt_pwd))
+
+  if [ (_git_branch_name) ]
+    set -l git_branch $red(_git_branch_name)
+    set git_info "$blue git:($git_branch$blue)"
+
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+    end
+  end
+
+  echo -n -s $arrow $cyan $cwd $git_info $normal ' '
+end
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
@@ -12,7 +53,7 @@ set fish_theme robbyrussell
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
-source $fish_path/oh-my-fish.fish
+. $fish_path/oh-my-fish.fish
 
 # Environment Variable
 
