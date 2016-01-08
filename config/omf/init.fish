bea7164a74349755370cf9679dc85f59bba1d030
Coeuvre Wong <coeuvre@gmail.com>
2016-01-08 19:16:12 +0000

    Updated

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -3,15 +3,15 @@
 # Environment Variable
 #
 ###############################################################################
-set -x PATH ~/.local/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
+set -x PATH ~/.multirust/toolchains/nightly/cargo/bin ~/.local/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 set -x MANPATH /usr/local/share/man $MANPATH
 
 # C/C++
-set -x C_INCLUDE_PATH ~/.local/include /usr/local/include $C_INCLUDE_PATH
-set -x CPLUS_INCLUDE_PATH $C_INCLUDE_PATH $CPLUS_INCLUDE_PATH
-set -x LIBRARY_PATH ~/.local/lib /usr/local/lib $LIBRARY_PATH
-set -x LD_LIBRARY_PATH $LIBRARY_PATH $LD_LIBRARY_PATH
-set -x PKG_CONFIG_PATH ~/.local/lib/pkgconfig /usr/local/lib/pkgconfig $PKG_CONFIG_PATH
+set -x C_INCLUDE_PATH ~/.local/include:/usr/local/include
+set -x CPLUS_INCLUDE_PATH $C_INCLUDE_PATH
+set -x LIBRARY_PATH ~/.local/lib:/usr/local/lib
+set -x LD_LIBRARY_PATH $LIBRARY_PATH
+set -x PKG_CONFIG_PATH ~/.local/lib/pkgconfig:/usr/local/lib/pkgconfig
 
 switch (uname)
     case Linux
