478253280c133c88d5c1acf05f286d4e00e9025a
Coeuvre Wong <coeuvre@gmail.com>
2016-01-29 22:48:11 +0000

    Updated

--- config/omf/before.init.fish
+++ config/omf/before.init.fish
@@ -1,1 +1,6 @@
-set -g Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
+switch (uname)
+    case Darwin
+        set -g Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
+    case '*'
+        set -g Z_SCRIPT_PATH ~/.local/Cellar/z/z.sh
+end
