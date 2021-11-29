9c60427586c0f7bf8cfe351ce5d0d71b60affb12
Chi Wang <coeuvre@gmail.com>
2021-11-29 22:56:12 +0100

    Updated

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,6 +29,8 @@ vim.cmd [[
   set backup
   set updatetime=300 " Reduce time for highlighting other references
   set redrawtime=10000 " Allow more time for loading syntax on large files
+  set inccommand=nosplit
+  set pumheight=8
 ]]
 
 local backupdir = vim.fn.stdpath("data").."/backup//"
@@ -165,7 +167,60 @@ require("packer").startup {
     use { "lewis6991/spellsitter.nvim", config = function() require("spellsitter").setup() end }
 
     -- nvim-lsp
-    use "neovim/nvim-lspconfig"
+    use {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+        local lsp_status = require('lsp-status')
+        lsp_status.register_progress()
+        capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
+
+        local on_attach = function(client)
+          local key_map = vim.api.nvim_buf_set_keymap
+
+          local opts = { noremap = true, silent = true }
+          key_map(bufnr, "n", "gd", [[<Cmd>lua vim.lsp.buf.definition()<CR>]], opts)
+          key_map(bufnr, "n", "gD", [[<Cmd>lua vim.lsp.buf.declaration()<CR>]], opts)
+          key_map(bufnr, "n", "gi", [[<Cmd>lua vim.lsp.buf.implementation()<CR>]], opts)
+          key_map(bufnr, "n", "gr", [[<Cmd>lua vim.lsp.buf.references()<CR>]], opts)
+          key_map(bufnr, "n", "<space>ca", [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], opts)
+
+          key_map(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+          key_map(bufnr, "i", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
+          key_map(bufnr, "n", "<F2>", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+          key_map(bufnr, "n", "[d", "<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+          key_map(bufnr, "n", "]d", "<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+
+          key_map(bufnr, "n", "<leader>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+          key_map(bufnr, "n", "<leader>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+          key_map(bufnr, "n", "<leader>wl", "<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+          key_map(bufnr, "n", "<leader>wd", [[<Cmd>lua require("telescope.builtin").lsp_workspace_diagnostics()<CR>]], opts)
+
+          require("illuminate").on_attach(client)
+          lsp_status.on_attach(client)
+        end
+
+        -- lspconfig.clangd.setup {
+        --     on_attach = on_attach,
+        --     capabilities = capabilities,
+        --     handlers = lsp_status.extensions.clangd.setup(),
+        --     init_options = {
+        --       clangdFileStatus = true
+        --     },
+        -- }
+
+        local servers = { "rust_analyzer", "zls", "jsonls" }
+        for _, server in ipairs(servers) do
+          lspconfig[server].setup {
+            on_attach = on_attach,
+            capabilities = capabilities
+          }
+        end
+      end,
+    }
     use "nvim-lua/lsp-status.nvim"
     use {
       "onsails/lspkind-nvim",
@@ -293,57 +348,6 @@ require("packer").startup {
             { name = "cmdline" }
           })
         })
-
-        -- Setup lspconfig.
-        local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-        local lspconfig = require("lspconfig")
-
-        local lsp_status = require('lsp-status')
-        lsp_status.register_progress()
-        capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
-
-        local on_attach = function(client)
-          local key_map = vim.api.nvim_buf_set_keymap
-
-          local opts = { noremap = true, silent = true }
-          key_map(bufnr, "n", "gd", [[<Cmd>lua vim.lsp.buf.definition()<CR>]], opts)
-          key_map(bufnr, "n", "gD", [[<Cmd>lua vim.lsp.buf.declaration()<CR>]], opts)
-          key_map(bufnr, "n", "gi", [[<Cmd>lua vim.lsp.buf.implementation()<CR>]], opts)
-          key_map(bufnr, "n", "gr", [[<Cmd>lua vim.lsp.buf.references()<CR>]], opts)
-          key_map(bufnr, "n", "<space>ca", [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], opts)
-
-          key_map(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-          key_map(bufnr, "i", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-
-          key_map(bufnr, "n", "<F2>", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-          key_map(bufnr, "n", "[d", "<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-          key_map(bufnr, "n", "]d", "<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-
-          key_map(bufnr, "n", "<leader>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-          key_map(bufnr, "n", "<leader>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-          key_map(bufnr, "n", "<leader>wl", "<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-          key_map(bufnr, "n", "<leader>wd", [[<Cmd>lua require("telescope.builtin").lsp_workspace_diagnostics()<CR>]], opts)
-
-          require("illuminate").on_attach(client)
-          lsp_status.on_attach(client)
-        end
-
-        lspconfig.clangd.setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            handlers = lsp_status.extensions.clangd.setup(),
-            init_options = {
-              clangdFileStatus = true
-            },
-        }
-
-        local other_servers = { "rust_analyzer", "zls", "jsonls" }
-        for _, server in ipairs(other_servers) do
-          lspconfig[server].setup {
-            on_attach = on_attach,
-            capabilities = capabilities
-          }
-        end
       end,
     }
     use "hrsh7th/cmp-nvim-lsp"
@@ -355,12 +359,25 @@ require("packer").startup {
     use {
       "windwp/nvim-autopairs",
       config = function()
-        require("nvim-autopairs").setup()
+        require("nvim-autopairs").setup {
+          check_ts = true,
+        }
 
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
         local cmp = require("cmp")
         cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({  map_char = { tex = "" } }))
       end,
+    }
+
+    -- Task
+    use "skywind3000/asynctasks.vim"
+    use {
+      "skywind3000/asyncrun.vim",
+      config = function()
+        if vim.fn.eval [[ has('win32') ]] == 1 then
+          vim.g.asyncrun_encs = 'gbk'
+        end
+      end
     }
 
     -- Automatically set up your configuration after cloning packer.nvim
@@ -412,6 +429,9 @@ nnoremap n nzzzv
 nnoremap N Nzzzv
 nnoremap J mzJ`z
 
+" Clear highlight search
+nnoremap <silent> <Esc><Esc> :nohls<cr>
+
 cmap w!! %!sudo tee > /dev/null %
 ]]
 
@@ -420,7 +440,7 @@ cmap w!! %!sudo tee > /dev/null %
 key_map("n", "<leader>ff", [[<Cmd>lua require"telescope.builtin".file_browser()<CR>]], { noremap = true, silent = true })
 
 -- find files
-key_map("n", "<leader><leader>", [[<Cmd>lua require"telescope.builtin".find_files { hidden = true }<CR>]], { noremap = true, silent = true })
+key_map("n", "<leader><leader>", [[<Cmd>lua require"telescope.builtin".find_files()<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fF", [[<Cmd>lua require"telescope.builtin".find_files { hidden=true, no_ignore=true }<CR>]], { noremap = true, silent = true })
 
 
@@ -464,3 +484,39 @@ key_map("n", "<leader>hm", [[<Cmd>lua require"telescope.builtin".keymaps()<CR>]]
 
 -- projects
 key_map("n", "<leader>pp", [[<Cmd>lua require"telescope".extensions.projects.projects()<CR>]], { noremap = true, silent = true })
+
+-- async tasks
+key_map('n', '<leader>pr', ":AsyncTask project-run<CR>", { noremap = true, silent = true })
+key_map('n', '<leader>pb', ":AsyncTask project-build<CR>", { noremap = true, silent = true })
+
+_G.open_quickfix = function(opts)
+  local opts = opts or {}
+  local toggle = opts.toggle or false
+
+  local is_open = false
+  for _, wininfo in ipairs(vim.fn.getwininfo()) do
+    if wininfo.quickfix == 1 then
+      is_open = true
+      break
+    end
+  end
+
+  if is_open then
+    if toggle then
+      vim.cmd [[ cclose ]]
+    end
+  else
+    local winnr = vim.fn.winnr()
+    local view = vim.fn.winsaveview()
+    vim.cmd [[ bot copen ]]
+
+    -- restore old window view
+    vim.cmd( winnr .. " " .. [[ wincmd w ]])
+    vim.fn.winrestview(view)
+  end
+end
+
+vim.cmd [[ autocmd User AsyncRunStart lua open_quickfix() ]]
+--vim.cmd [[ autocmd FileType qf nnoremap <buffer> <silent> <C-c> :cclose<CR> ]]
+
+key_map('n', '<C-q>', [[<cmd>lua open_quickfix { toggle = true }<CR>]], { noremap = true, silent = true })
