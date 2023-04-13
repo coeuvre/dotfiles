53fef6def54f3aa7ab03f480497bee8b8db739d7
Chi Wang <coeuvre@gmail.com>
2023-04-14 00:46:00 +0200

    Updated

---
+++ config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- disable intent guide
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+}
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    if vim.fn.has("win32") then
+      opts.ensure_installed = {}
+      return
+    end
+
     -- workaroudn for https://github.com/LazyVim/LazyVim/issues/524
     opts.ignore_install = { "help" }
 
