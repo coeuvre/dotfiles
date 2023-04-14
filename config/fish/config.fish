55e62e8dd3c24ccd6255d33b4442fae3ad233bc7
Chi Wang <chiwang@google.com>
2023-04-14 14:15:39 +0200

    Updated

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,49 +1,7 @@
-###############################################################################
-#
-# Alias
-#
-###############################################################################
-alias l ls
-alias grep "grep --color"
+set -x PATH ~/bin ~/.cargo/bin $PATH
 
-switch (uname)
-    case Darwin
-        # alias to love
-        alias love "/Applications/love.app/Contents/MacOS/love"
+alias copybara /google/bin/releases/copybara/public/copybara/copybara
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
-
-###############################################################################
-#
-# Environment Variable
-#
-###############################################################################
-if test $FISH_RC_LOADED
-    exit
-end
-
-set -x FISH_RC_LOADED true
-
-set -x PATH ~/.local/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin $PATH
-set -x MANPATH /usr/local/share/man $MANPATH
-
-# C/C++
-set -x C_INCLUDE_PATH ~/.local/include:/usr/local/include
-set -x CPLUS_INCLUDE_PATH $C_INCLUDE_PATH
-set -x LIBRARY_PATH ~/.local/lib:/usr/local/lib
-set -x LD_LIBRARY_PATH $LIBRARY_PATH
-set -x PKG_CONFIG_PATH ~/.local/lib/pkgconfig:/usr/local/lib/pkgconfig
-
-switch (uname)
-    case Linux
-        set -x PATH ~/.linuxbrew/bin $PATH
-        set -x MANPATH ~/.linuxbrew/share/man $MANPATH
-        set -x INFOPATH ~/.linuxbrew/share/info $INFOPATH
-    case Darwin # MAC OS X
-        set -x JAVA_HOME (/usr/libexec/java_home)
-        set -x DYLD_LIBRARY_PATH ~/.local/lib $DYLD_LIBRARY_PATH
-    case 'CYGWIN*'
-        set -x PATH ~/.local/Cellar/rust/bin $PATH
-end
-
-# SSH
-set -x SSH_KEY_PATH "~/.ssh/id_rsa"
