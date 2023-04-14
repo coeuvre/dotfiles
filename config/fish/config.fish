bfa80e9abb673281cc41819877c610b7fcc2ea37
Chi Wang <chiwang@google.com>
2023-04-14 14:24:53 +0200

    Updated

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,5 @@
+fish_config theme choose tokyonight_day
+
 set -x PATH ~/bin ~/.cargo/bin $PATH
 
-alias copybara /google/bin/releases/copybara/public/copybara/copybara
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+test -e config.local.fish && source config.local.fish
