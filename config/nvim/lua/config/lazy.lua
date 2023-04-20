fa0529b1163ce68a4d8bae025a4d51017b370ab0
Chi Wang <chiwang@google.com>
2023-04-20 14:03:49 +0200

    updated

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -27,8 +27,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  install = { colorscheme = {} },
   performance = {
     rtp = {
       -- disable some rtp plugins
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
+    "williamboman/mason.nvim",
     opts = {
       ensure_installed = {},
     },
