7ff2041e109fb33355c0e6dcfa532c85cb9c5fea
Chi Wang <chiwang@google.com>
2023-05-17 22:00:10 +0200

    Updated

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,4 +3,6 @@ set -x PATH ~/bin ~/.cargo/bin $PATH
 # Colorful ls on macOS
 set -x CLICOLOR 1
 
+alias em="emacs -nw"
+
 test -e ~/.config/fish/config.local.fish && source ~/.config/fish/config.local.fish
