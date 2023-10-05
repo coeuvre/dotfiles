068ddd5fcd1dda775a1a8b5c5dbf67afb463d99f
Chi Wang <coeuvre@gmail.com>
2023-10-05 23:52:56 +0200

    Updated

---
+++ wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "catppuccin-mocha"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  {
+    key = 'Space',
+    mods = 'CTRL',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "C:\\msys64\\usr\\bin\\fish.exe", "--login", "-i" }
+	config.set_environment_variables = {
+		["CHERE_INVOKING"] = "1",
+		["MSYS2_PATH_TYPE"] = "inherit",
+	}
+end
+
+return config
