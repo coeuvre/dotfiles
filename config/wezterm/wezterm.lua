abcc67e543ef1bdde3d36d73972920efcfe79e54
Chi Wang <chiwang@google.com>
2024-01-08 06:08:30 +0100

    updated

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,33 +15,93 @@ config.font = wezterm.font("JetBrainsMono NF")
 config.keys = {
 	{
 		key = "h",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.ActivatePaneDirection("Left"),
 	},
 	{
 		key = "l",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.ActivatePaneDirection("Right"),
 	},
 	{
 		key = "j",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.ActivatePaneDirection("Down"),
 	},
 	{
 		key = "k",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.ActivatePaneDirection("Up"),
 	},
 	{
 		key = "v",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
 		key = "s",
-		mods = "CTRL|SHIFT",
+		mods = "CTRL|ALT",
 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "t",
+		mods = "CTRL|ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "1",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(0),
+	},
+	{
+		key = "2",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(1),
+	},
+	{
+		key = "3",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(2),
+	},
+	{
+		key = "4",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(3),
+	},
+	{
+		key = "5",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(4),
+	},
+	{
+		key = "6",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(5),
+	},
+	{
+		key = "7",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(6),
+	},
+	{
+		key = "8",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(7),
+	},
+	{
+		key = "9",
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(-1),
+	},
+	{
+		key = "[",
+		mods = "CTRL|ALT",
+		action = act.ActivateTabRelative(-1),
+	},
+	{
+		key = "]",
+		mods = "CTRL|ALT",
+		action = act.ActivateTabRelative(1),
 	},
 }
 
