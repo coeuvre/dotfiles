7308a182fd8a0ab323496b6761cb8269cc4fdf26
Chi Wang <coeuvre@gmail.com>
2024-03-04 08:29:53 +0100

    Revert "updated"
    This reverts commit 08a3e0343cae9756e920109b6bc5716875c2b511.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
+
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
@@ -11,171 +12,106 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("JetBrainsMono NF")
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-
-local function is_vim(pane)
-    -- this is set by the plugin, and unset on ExitPre in Neovim
-    return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-    h = "Left",
-    j = "Down",
-    k = "Up",
-    l = "Right",
-}
-
-local function split_nav(key)
-    return {
-        key = key,
-        mods = "CTRL",
-        action = wezterm.action_callback(function(win, pane)
-            if is_vim(pane) then
-                -- pass the keys through to vim/nvim
-                win:perform_action({
-                    SendKey = { key = key, mods = "CTRL" },
-                }, pane)
-            else
-                win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-            end
-        end),
-    }
+local mods = "CTRL|ALT"
+if string.find(wezterm.target_triple, "apple") ~= nil then
+    mods = "CTRL|CMD"
 end
 
 config.keys = {
     {
-        mods = "LEADER",
-        key = "a",
-        action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+        key = "h",
+        mods = mods,
+        action = act.ActivatePaneDirection("Left"),
     },
     {
-        mods = "LEADER|CTRL",
-        key = "a",
-        action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-    },
-    {
-        mods = "LEADER",
         key = "l",
-        action = wezterm.action.SendKey({ key = "l", mods = "CTRL" }),
+        mods = mods,
+        action = act.ActivatePaneDirection("Right"),
     },
     {
-        mods = "LEADER|CTRL",
-        key = "l",
-        action = wezterm.action.SendKey({ key = "l", mods = "CTRL" }),
-    },
-
-    -- splitting
-    {
-        mods = "LEADER",
-        key = "s",
-        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+        key = "j",
+        mods = mods,
+        action = act.ActivatePaneDirection("Down"),
     },
     {
-        mods = "LEADER|CTRL",
-        key = "s",
-        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+        key = "k",
+        mods = mods,
+        action = act.ActivatePaneDirection("Up"),
     },
     {
-        mods = "LEADER",
         key = "v",
-        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+        mods = mods,
+        action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
     },
     {
-        mods = "LEADER|CTRL",
-        key = "v",
-        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+        key = "s",
+        mods = mods,
+        action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
     },
-
-    -- move between split panes
-    split_nav("h"),
-    split_nav("j"),
-    split_nav("k"),
-    split_nav("l"),
-
     {
-        mods = "LEADER",
-        key = "c",
+        key = "t",
+        mods = mods,
         action = act.SpawnTab("CurrentPaneDomain"),
     },
     {
-        mods = "LEADER|CTRL",
-        key = "c",
-        action = act.SpawnTab("CurrentPaneDomain"),
-    },
-    {
-        mods = "LEADER",
-        key = "p",
-        action = act.ActivateTabRelative(-1),
-    },
-    {
-        mods = "LEADER|CTRL",
-        key = "p",
-        action = act.ActivateTabRelative(-1),
-    },
-    {
-        mods = "LEADER",
-        key = "n",
-        action = act.ActivateTabRelative(1),
-    },
-    {
-        mods = "LEADER|CTRL",
-        key = "n",
-        action = act.ActivateTabRelative(1),
-    },
-    {
-        mods = "LEADER",
         key = "1",
+        mods = mods,
         action = act.ActivateTab(0),
     },
     {
-        mods = "LEADER",
         key = "2",
+        mods = mods,
         action = act.ActivateTab(1),
     },
     {
-        mods = "LEADER",
         key = "3",
+        mods = mods,
         action = act.ActivateTab(2),
     },
     {
-        mods = "LEADER",
         key = "4",
+        mods = mods,
         action = act.ActivateTab(3),
     },
     {
-        mods = "LEADER",
         key = "5",
+        mods = mods,
         action = act.ActivateTab(4),
     },
     {
-        mods = "LEADER",
         key = "6",
+        mods = mods,
         action = act.ActivateTab(5),
     },
     {
-        mods = "LEADER",
         key = "7",
+        mods = mods,
         action = act.ActivateTab(6),
     },
     {
-        mods = "LEADER",
         key = "8",
+        mods = mods,
         action = act.ActivateTab(7),
     },
     {
-        mods = "LEADER",
         key = "9",
+        mods = mods,
         action = act.ActivateTab(-1),
     },
     {
-        mods = "LEADER",
-        key = "/",
-        action = act.Search({ CaseSensitiveString = "" }),
+        key = "[",
+        mods = mods,
+        action = act.ActivateTabRelative(-1),
     },
     {
-        mods = "LEADER",
-        key = "[",
-        action = wezterm.action.ActivateCopyMode,
+        key = "]",
+        mods = mods,
+        action = act.ActivateTabRelative(1),
+    },
+    {
+        key = "f",
+        mods = mods,
+        action = act.Search({ CaseSensitiveString = "" }),
     },
 }
 
