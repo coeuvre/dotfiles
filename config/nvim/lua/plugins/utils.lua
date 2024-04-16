c4c4c99c6c07e39e910ebd0ff06c73990216e386
Chi Wang <coeuvre@gmail.com>
2024-04-16 23:56:48 +0200

    Updated

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -189,10 +189,6 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-            },
         },
         config = function()
             local telescope = require("telescope")
@@ -223,15 +219,7 @@ return {
                         previewer = false,
                     },
                 },
-                extensions = {
-                    fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                    },
-                },
             })
-            telescope.load_extension("fzf")
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
