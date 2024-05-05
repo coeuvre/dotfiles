98bf21dc46a6eb939c25e2bf23ad8109aac9b0fa
Chi Wang <coeuvre@gmail.com>
2024-05-05 10:23:54 +0200

    Updated.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ end
 config.color_scheme = "catppuccin-mocha"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font("JetBrainsMono NFM")
+config.font = wezterm.font("JetBrainsMonoNL NFP")
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { "C:\\msys64\\usr\\bin\\fish.exe", "--login", "-i" }
