f2c1d72638c53b000fa7ecdb4f12dddfbcf2badc
Chi Wang <coeuvre@gmail.com>
2024-06-07 21:00:32 +0200

    Updated.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -96,6 +96,7 @@ return {
             }),
         })
 
+        require("nvim-autopairs").setup({})
         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
         cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
