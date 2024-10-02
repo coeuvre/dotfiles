b55e6fcceb00c4542bcd8049278a2e8969e8f0df
Chi Wang <chiwang@google.com>
2024-10-02 14:51:50 +0200

    Updated.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,8 +8,80 @@ end
 
 config.color_scheme = "catppuccin-mocha"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 config.font = wezterm.font("JetBrainsMonoNL NFP")
+config.use_dead_keys = false
+
+local mods = "CTRL | ALT"
+if string.find(wezterm.target_triple, "darwin") then
+    mods = "CTRL | CMD"
+end
+
+config.keys = {
+    {
+        key = "x",
+        mods = mods,
+        action = wezterm.action.ActivateCopyMode,
+    },
+    {
+        key = "z",
+        mods = mods,
+        action = wezterm.action.TogglePaneZoomState,
+    },
+
+    -- panes
+    {
+        key = "s",
+        mods = mods,
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = "v",
+        mods = mods,
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = "h",
+        mods = mods,
+        action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+        key = "j",
+        mods = mods,
+        action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+    {
+        key = "k",
+        mods = mods,
+        action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+        key = "l",
+        mods = mods,
+        action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+
+    -- tabs
+    {
+        key = "t",
+        mods = mods,
+        action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+        key = "[",
+        mods = mods,
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+    {
+        key = "]",
+        mods = mods,
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+}
+
+for i = 1, 9 do
+    table.insert(config.keys, { key = tostring(i), mods = mods, action = wezterm.action.ActivateTab(i - 1) })
+end
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { "C:\\msys64\\usr\\bin\\fish.exe", "--login", "-i" }
