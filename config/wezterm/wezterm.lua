cd946338b4064ff0b6675188ff38e805e6802480
Chi Wang <coeuvre@gmail.com>
2024-10-06 11:08:04 +0200

    Updated.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,8 +6,6 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
--- Disable wayland support until the rewrite is done
-config.enable_wayland = false
 config.color_scheme = "catppuccin-mocha"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.font = wezterm.font("JetBrainsMonoNL NFP")
