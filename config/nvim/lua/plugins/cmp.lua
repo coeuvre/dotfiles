e87e399c0d764baf8c7e1283fa8e91596a00f083
Chi Wang <chiwang@google.com>
2024-11-15 11:20:51 +0100

    Updated.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -3,6 +3,8 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
 
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
@@ -90,8 +92,11 @@ return {
             },
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
+                { name = "nvim_lsp_signature_help" },
+            }, {
                 { name = "luasnip" },
             }, {
+                { name = "path" },
                 {
                     name = "buffer",
                     option = {
