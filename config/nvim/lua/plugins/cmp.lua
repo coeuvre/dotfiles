ea50118942726d250db538bd8546cadaec211f8e
Chi Wang <coeuvre@gmail.com>
2024-12-18 19:41:58 +0100

    Updated.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,116 +1,12 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-
-        "onsails/lspkind.nvim",
-
-        "windwp/nvim-autopairs",
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    opts = {
+        keymap = { preset = "super-tab" },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+        signature = { enabled = true },
     },
-
-    config = function()
-        local luasnip = require("luasnip")
-        local cmp = require("cmp")
-        local lspkind = require("lspkind")
-
-        local confirm = function(fallback)
-            if cmp.visible() then
-                local entry = cmp.get_selected_entry()
-                if not entry then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                end
-                cmp.confirm()
-                return
-            end
-
-            if luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-                return
-            end
-
-            fallback()
-        end
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                }),
-            },
-            preselect = cmp.PreselectMode.Item,
-            completion = {
-                completeopt = "menu,menuone,noinsert",
-            },
-            mapping = {
-                ["<C-e>"] = cmp.mapping.abort(),
-
-                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                ["<C-p>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                        return
-                    end
-
-                    fallback()
-                end, { "i", "s" }),
-
-                ["<C-n>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        return
-                    end
-
-                    fallback()
-                end, { "i", "s" }),
-
-                ["<Cr>"] = cmp.mapping(confirm, { "i", "s" }),
-
-                ["<Tab>"] = cmp.mapping(confirm, { "i", "s" }),
-
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                        return
-                    end
-
-                    fallback()
-                end, { "i", "s" }),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "nvim_lsp_signature_help" },
-            }, {
-                { name = "luasnip" },
-            }, {
-                { name = "path" },
-                {
-                    name = "buffer",
-                    option = {
-                        keyword_pattern = [[\k\+]],
-                        get_bufnrs = function()
-                            return vim.api.nvim_list_bufs()
-                        end,
-                    },
-                },
-            }),
-        })
-
-        require("nvim-autopairs").setup({})
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
 }
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -9,6 +9,18 @@ return {
 
         "j-hui/fidget.nvim",
         "SmiteshP/nvim-navic",
+
+        "saghen/blink.cmp",
+
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
 
     config = function()
@@ -21,52 +33,28 @@ return {
             },
         })
 
+        local servers = {
+            lua_ls = {},
+
+            clangd = {
+                autostart = false,
+                capabilities = {
+                    offsetEncoding = "utf-16",
+                },
+            },
+
+            ts_ls = {},
+            eslint = {},
+
+            zls = {},
+
+            rust_analyzer = {},
+        }
+
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-            on_init = function(client)
-                local path = client.workspace_folders[1].name
-                if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-                    return
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME,
-                            -- Depending on the usage, you might want to add additional paths here.
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        },
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    },
-                })
-            end,
-            settings = {
-                Lua = {},
-            },
-        })
-
-        lspconfig.clangd.setup({
-            autostart = false,
-            capabilities = {
-                offsetEncoding = "utf-16",
-            },
-        })
-
-        lspconfig.ts_ls.setup({})
-
-        lspconfig.eslint.setup({})
-
-        lspconfig.zls.setup({})
-
-        lspconfig.rust_analyzer.setup({})
+        for server, config in pairs(servers) do
+            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
     end,
 }
