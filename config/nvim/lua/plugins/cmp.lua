a9b66343d23ac4f7628288bc3e454310a6baff51
Chi Wang <coeuvre@gmail.com>
2025-01-01 10:21:25 +0100

    Updated.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -6,10 +6,30 @@ return {
             "windwp/nvim-autopairs",
             opts = {},
         },
+        { "L3MON4D3/LuaSnip", version = "v2.*" },
     },
     version = "v0.*",
     opts = {
-        keymap = { preset = "super-tab" },
+        keymap = {
+            preset = "super-tab",
+        },
         signature = { enabled = true },
+        snippets = {
+            expand = function(snippet)
+                require("luasnip").lsp_expand(snippet)
+            end,
+            active = function(filter)
+                if filter and filter.direction then
+                    return require("luasnip").jumpable(filter.direction)
+                end
+                return require("luasnip").in_snippet()
+            end,
+            jump = function(direction)
+                require("luasnip").jump(direction)
+            end,
+        },
+        sources = {
+            default = { "lsp", "path", "luasnip", "buffer" },
+        },
     },
 }
