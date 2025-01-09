271f4c48a9da583d3443ea958533e173997a5ab5
Chi Wang <chiwang@google.com>
2025-01-09 16:47:17 +0100

    Updated.

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -4,11 +4,12 @@ return {
 
     config = function()
         require("mini.basics").setup()
+        require("mini.comment").setup()
 
         require("mini.ai").setup()
         require("mini.pairs").setup()
         require("mini.surround").setup()
-        require("mini.comment").setup()
+
         require("mini.bracketed").setup()
         require("mini.jump").setup()
         require("mini.jump2d").setup()
@@ -21,20 +22,5 @@ return {
         require("mini.git").setup()
         require("mini.statusline").setup()
         require("mini.icons").setup()
-        require("mini.trailspace").setup()
-
-        local hipatterns = require("mini.hipatterns")
-        hipatterns.setup({
-            highlighters = {
-                -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-                fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-                hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-                todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-                note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-
-                -- Highlight hex color strings (`#rrggbb`) using that color
-                hex_color = hipatterns.gen_highlighter.hex_color(),
-            },
-        })
     end,
 }
