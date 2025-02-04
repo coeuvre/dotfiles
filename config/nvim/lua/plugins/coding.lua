93da1eddd2b46799cf3e9a27f3a05a5891a314f7
Chi Wang <chiwang@google.com>
2025-02-04 14:41:09 +0100

    Updated

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -4,6 +4,11 @@ return {
     keymap = {
       preset = "super-tab",
     },
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
     fuzzy = {
       max_typos = function()
         return 0
