6327598d0b58006c92f80854ea341f736805b8b5
Chi Wang <coeuvre@gmail.com>
2025-04-03 21:37:25 +0200

    Updated.

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -5,7 +5,34 @@ return {
       keymap = {
         preset = "super-tab",
       },
+
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = false,
+          },
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+
+      fuzzy = {
+        implementation = "lua",
+      },
+
       signature = { enabled = true },
+
+      sources = {
+        default = { "lsp", "buffer", "path" },
+
+        -- disable snippets
+        transform_items = function(_, items)
+          return vim.tbl_filter(function(item)
+            return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
+          end, items)
+        end,
+      },
     },
   },
 }
