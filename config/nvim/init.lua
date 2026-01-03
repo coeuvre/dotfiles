0274c8f4355706ee3bcf25baed704dbb9385f7ad
Chi Wang <coeuvre@gmail.com>
2026-01-04 00:06:09 +0100

    Updated.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -61,6 +61,10 @@ vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save", silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+if vim.fn.has("win32") == 1 then
+  -- Also map <BS> on Windows since we cannot distinguish them.
+  vim.keymap.set("n", "<BS>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+end
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
