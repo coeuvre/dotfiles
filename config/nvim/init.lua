2499886a3ff733b546842f2f04978ea49d36d495
Chi Wang <coeuvre@gmail.com>
2026-07-15 11:53:14 +0200

    make neovim dumb.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -41,9 +41,6 @@ opt.virtualedit = "block"
 opt.winminwidth = 5
 opt.wrap = false
 
-vim.g.zig_fmt_parse_errors = 0
-vim.g.zig_fmt_autosave = 0
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -98,20 +95,6 @@ end
 vim.keymap.set("n", "]d", diagnostic_jump(1), { desc = "Next Diagnostic" })
 vim.keymap.set("n", "[d", diagnostic_jump(-1), { desc = "Prev Diagnostic" })
 
-vim.api.nvim_create_user_command("FixAll", function()
-  vim.lsp.buf.code_action({
-    context = { only = { "source.fixAll" } },
-    apply = true,
-  })
-end, { desc = "LSP Fix All" })
-
-vim.api.nvim_create_user_command("OrganizeImports", function()
-  vim.lsp.buf.code_action({
-    context = { only = { "source.organizeImports" } },
-    apply = true,
-  })
-end, { desc = "LSP Organize Imports" })
-
 --------------------------------------------------------------------------------
 --- Plugins
 --------------------------------------------------------------------------------
@@ -160,51 +143,6 @@ local plugins = {
           ["<C-p>"] = false,
         },
       })
-    end,
-  },
-  {
-    src = "https://github.com/williamboman/mason.nvim",
-    setup = function()
-      require("mason").setup({})
-    end,
-  },
-  {
-    src = "https://github.com/MagicDuck/grug-far.nvim",
-    setup = function()
-      require("grug-far").setup({})
-    end,
-  },
-
-  -- Formatting ----------------------------------------------------------------
-  {
-    src = "https://github.com/stevearc/conform.nvim",
-    setup = function()
-      local web_formatter = { "prettierd", "prettier", stop_after_first = true }
-      require("conform").setup({
-        format_on_save = {
-          timeout_ms = 1000,
-          lsp_fallback = true,
-        },
-
-        formatters_by_ft = {
-          c = { "clang-format" },
-          cmake = { "cmake_format" },
-          cpp = { "clang-format" },
-          go = { "gofmt" },
-          html = web_formatter,
-          javascript = web_formatter,
-          javascriptreact = web_formatter,
-          lua = { "stylua" },
-          python = { "ruff_format" },
-          rust = { "rustfmt" },
-          sh = { "shfmt" },
-          typescriptreact = web_formatter,
-          typescript = web_formatter,
-          zig = { "zigfmt" },
-        },
-      })
-
-      vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
 
@@ -280,18 +218,10 @@ local plugins = {
         keymap = {
           preset = "enter",
           ["<C-e>"] = {
-            function()
-              -- TODO: Cancel inline completion
-            end,
             "hide",
             "fallback",
           },
           ["<Tab>"] = {
-            function()
-              if vim.lsp.inline_completion.get() then
-                return true
-              end
-            end,
             "select_and_accept",
             "fallback",
           },
@@ -311,7 +241,7 @@ local plugins = {
         },
         signature = { enabled = true },
         sources = {
-          default = { "lsp", "buffer", "path" },
+          default = { "buffer", "path" },
           transform_items = function(_, items)
             return vim.tbl_filter(function(item)
               return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
@@ -319,103 +249,6 @@ local plugins = {
           end,
         },
       })
-    end,
-  },
-
-  -- LSP -----------------------------------------------------------------------
-  {
-    src = "https://github.com/neovim/nvim-lspconfig",
-    setup = function()
-      local lsp_base_config = {
-        capabilities = require("blink.cmp").get_lsp_capabilities({
-          textDocument = { completion = { completionItem = { snippetSupport = false } } },
-        }),
-      }
-
-      local lsps = {
-        basedpyright = {},
-        clangd = { cmd = { "clangd", "--header-insertion=never" } },
-        gopls = {},
-        lua_ls = {},
-        rust_analyzer = {},
-        ts_ls = {},
-        zls = {},
-      }
-
-      for lsp, config in pairs(lsps) do
-        vim.lsp.config(lsp, vim.tbl_extend("force", lsp_base_config, config))
-        vim.lsp.enable(lsp)
-      end
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-          if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
-            vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-          end
-        end,
-      })
-
-      vim.api.nvim_create_user_command("ToggleCopilot", function()
-        if vim.lsp.is_enabled("copilot") then
-          vim.lsp.enable("copilot", false)
-          vim.notify("Copilot Disabled", vim.log.levels.INFO)
-        else
-          vim.lsp.enable("copilot")
-          vim.notify("Copilot Enabled", vim.log.levels.INFO)
-        end
-      end, { desc = "Toggle Copilot" })
-    end,
-  },
-
-  -- Treesitter ----------------------------------------------------------------
-  {
-    src = "https://github.com/nvim-treesitter/nvim-treesitter",
-    version = "main",
-    setup = function()
-      local parsers = { "bash", "c", "cpp", "lua", "vim", "vimdoc", "query", "zig" }
-
-      pcall(require("nvim-treesitter").install, parsers)
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = parsers,
-        callback = function()
-          -- syntax highlighting, provided by Neovim
-          vim.treesitter.start()
-          -- folds, provided by Neovim
-          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-          vim.wo.foldmethod = "expr"
-          -- indentation, provided by nvim-treesitter
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-  },
-
-  -- Async Tasks ---------------------------------------------------------------
-  "https://github.com/skywind3000/asyncrun.vim",
-  {
-    src = "https://github.com/skywind3000/asynctasks.vim",
-    setup = function()
-      vim.g.asyncrun_open = 10
-      vim.g.asyncrun_auto = "make"
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "AsyncRunStop",
-        callback = function()
-          vim.cmd("silent! cnext")
-        end,
-      })
-
-      vim.keymap.set("n", "<C-q>", function()
-        vim.cmd("call asyncrun#quickfix_toggle(10)")
-      end, { silent = true, desc = "Quickfix List" })
-
-      vim.keymap.set("n", "<leader>rb", ":AsyncTask build<cr>", { silent = true, desc = "AsyncTask build" })
-      vim.keymap.set("n", "<leader>rt", ":AsyncTask test<cr>", { silent = true, desc = "AsyncTask test" })
-      vim.keymap.set("n", "<leader>rr", ":AsyncTask run<cr>", { silent = true, desc = "AsyncTask run" })
     end,
   },
 }
@@ -431,13 +264,3 @@ for _, plugin in ipairs(plugins) do
   end
 end
 
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if kind == "install" or kind == "update" then
-      if name == "nvim-treesitter" then
-        vim.cmd("TSUpdate")
-      end
-    end
-  end,
-})
